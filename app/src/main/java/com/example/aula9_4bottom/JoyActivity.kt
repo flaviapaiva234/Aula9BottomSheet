39a5eb7a81e92adad8cc23851bd44ffef630baad
flavia <flaviapaiva234@gmail.com>
2022-06-12 21:45:12 -0300

    Third Commit Completed

---
+++ app/src/main/java/com/example/aula9_4bottom/JoyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aula9_4bottom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joy)
+    }
+}
